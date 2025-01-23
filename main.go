f8135b3f93db6ac84923bd3c57fab69207c46241
Samarth S <samarth.sudarshankumar@harness.io>
2025-01-22 18:01:53 -0700

    added api end point

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main() {
-	fmt.Println(HelloWorld())
+	"github.com/gin-gonic/gin"
+)
+
+// HelloWorld is a function that returns a string containing "hello world".
+func helloWorldApi(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 }
 
 // HelloWorld is a function that returns a string containing "hello world".
 func HelloWorld() string {
 	return "hello world"
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", helloWorldApi)
+
+	router.Run("localhost:8080")
 }
